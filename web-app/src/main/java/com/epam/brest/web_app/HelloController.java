4a7ed6bd9d0f3b4f765e61a1bebb19bd267db709
Andrei Klas <andreyklass94@gmail.com>
2021-12-12 23:49:29 +0300

    Try to add data input

--- web-app/src/main/java/com/epam/brest/web_app/HelloController.java
+++ web-app/src/main/java/com/epam/brest/web_app/HelloController.java
@@ -4,8 +4,9 @@ import com.epam.brest.model.Food;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -18,13 +19,11 @@ public class HelloController {
     private static List<Food> foodArrayList = new ArrayList<>();
 
 
-    public HelloController(){
+    public HelloController() {
         foodArrayList.add(new Food(1, "Pasta", 230));
         foodArrayList.add(new Food(2, "Potato", 120));
         foodArrayList.add(new Food(3, "Tomato", 80));
-
     }
-
 
 
     @GetMapping(value = "/orders")
@@ -33,5 +32,17 @@ public class HelloController {
 //        System.out.println("hello(name:'" + name + "')");
         model.addAttribute("foodsList", foodArrayList);
         return "orders";
+    }
+
+    @RequestMapping(value = "/addFood", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("food") Food food,
+                         BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        model.addAttribute("name", food.getFoodName());
+        model.addAttribute("cost", food.getFoodCost());
+        model.addAttribute("id", food.getFoodId());
+        return "employeeView";
     }
 }
