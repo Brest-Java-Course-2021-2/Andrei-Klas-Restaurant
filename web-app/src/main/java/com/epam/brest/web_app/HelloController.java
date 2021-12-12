c971ffbfeb59085224706516eac6b36243568d52
Andrei Klas <andreyklass94@gmail.com>
2021-12-12 20:37:28 +0300

    web part start working
    web part start working

--- web-app/src/main/java/com/epam/brest/web_app/HelloController.java
+++ web-app/src/main/java/com/epam/brest/web_app/HelloController.java
@@ -1,9 +1,13 @@
 package com.epam.brest.web_app;
 
+import com.epam.brest.model.Food;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
 
 /**
  * Hello MVC controller.
@@ -11,13 +15,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+    private static List<Food> foodArrayList = new ArrayList<>();
 
-    @GetMapping(value = "/hello")
-    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-                        Model model) {
 
-        System.out.println("hello(name:'" + name + "')");
-        model.addAttribute("name", name);
-        return "hello";
+    public HelloController(){
+        foodArrayList.add(new Food(1, "Pasta", 230));
+        foodArrayList.add(new Food(2, "Potato", 120));
+        foodArrayList.add(new Food(3, "Tomato", 80));
+
+    }
+
+
+
+    @GetMapping(value = "/orders")
+    public String orders(Model model) {
+
+//        System.out.println("hello(name:'" + name + "')");
+        model.addAttribute("foodsList", foodArrayList);
+        return "orders";
     }
 }
